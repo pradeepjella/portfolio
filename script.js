00f6cea7ce9c521bbcf912cf2504c51779845253
pradeepjella <pradeepjella624@gmail.com>
2025-10-08 16:45:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+  });
+});
